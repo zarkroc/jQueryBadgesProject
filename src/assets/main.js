9403638c32717746e72149128f9e2d8b7862fa08
zarkroc <9654120+zarkroc@users.noreply.github.com>
2018-01-11 10:35:17 +0100

    first commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,36 @@
 $(function() {
+    $.ajax({
+    url: 'https://www.codeschool.com/users/zarkroc.json',
+    dataType: 'jsonp',
+    success: function(response) {
+        addCourses(response.courses.completed);
+    }
+  });
 
-  // your code will go here
+    function addCourses(courses) {
+        var $badges = $('#badges');
+
+        courses.forEach(function(course) {
+            var $course = $('<div />', {
+                'class': 'course'
+            }).appendTo($badges);
+
+            $('<h3 />', {
+                text: course.title
+            }).appendTo($course);
+
+        $('<img />', {
+            src: course.badge
+        }).appendTo($course);
+
+        $('<a />', {
+            'class': 'btn btn-primary',
+            target: '_blank',
+            href: course.url,
+            text: 'See Course'
+        }).appendTo($course);
+        })
+    }
+
 
 });
